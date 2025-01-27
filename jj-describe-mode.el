2acc43b3a9adce4c505d9c0794d5459d2d8a67f5
Hauke Rehfeld <git@haukerehfeld.de>
2025-01-27 01:32:30 +0100

    add: detect .jj from default-directory or filepath

--- jj-describe-mode.el
+++ jj-describe-mode.el
@@ -37,16 +37,23 @@
   (let ((normalized-dirs (mapcar #'directory-file-name jj-describe-mode-temp-dirs)))
     (seq-some (lambda (dir) (file-in-directory-p default-directory dir)) normalized-dirs)))
 
+(defun jj-describe-mode-get-repo-root ()
+  (or (locate-dominating-file default-directory ".jj")
+      (locate-dominating-file (buffer-file-name) ".jj")))
+
 (defun jj-describe-mode-insert-status ()
   "Insert the output of `jj status` at the end of the buffer and comment it out."
-  (unless (jj-describe-mode-in-temp-dir-p)
-    (let ((output (shell-command-to-string "jj status")))
-      (if (string-empty-p output)
-          (message "Failed to retrieve status.")
-        (let ((end-pos (point-max)))
-          (goto-char end-pos)
-          (insert output)
-          (comment-region end-pos (point-max)))))))
+  (let ((root (jj-describe-mode-get-repo-root))
+        (point (point)))
+    (when root
+      (let ((output (shell-command-to-string "jj status")))
+        (if (string-empty-p output)
+            (user-error "Failed to retrieve status.")
+          (let ((end-pos (point-max)))
+            (goto-char end-pos)
+            (insert output)
+            (comment-region end-pos (point-max))
+            (goto-char point)))))))
 
 (defun jj-describe-mode-insert-info ()
   "Centralized function to insert additional info comments into the buffer."
